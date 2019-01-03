ece985018c62050419c37db8d2e3c36734db3452
Kyle Shores <kyle.shores44@gmail.com>
2019-01-03 15:37:42 -0600

    competition, officer, contact, perfomance page, removed footer

--- tambda/views.py
+++ tambda/views.py
@@ -6,8 +6,35 @@ def home(request):
 def lessons(request):
     return render(request, 'tambda/lessons.html', {})
 
-def competitions(request):
-    return render(request, 'tambda/competitions.html', {})
-
 def performance_team(request):
     return render(request, 'tambda/performance_team.html', {})
+
+def competition_home(request):
+    return render(request, 'tambda/competition_home.html', {})
+
+def competition_attire(request):
+    return render(request, 'tambda/competition_attire.html', {})
+
+def competition_packing_list(request):
+    return render(request, 'tambda/competition_packing_list.html', {})
+
+def competition_pictures(request):
+    return render(request, 'tambda/competition_pictures.html', {})
+
+def competition_results(request):
+    return render(request, 'tambda/competition_results.html', {})
+
+def competition_forms(request):
+    return render(request, 'tambda/competition_forms.html', {})
+
+def competition_syllabus(request):
+    return render(request, 'tambda/competition_syllabus.html', {})
+
+def competition_links(request):
+    return render(request, 'tambda/competition_links.html', {})
+
+def officers(request):
+    return render(request, 'tambda/officers.html', {})
+
+def contact_us(request):
+    return render(request, 'tambda/contact_us.html', {})
