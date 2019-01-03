e656ec0994745b06282a6952edd0c2d36254f682
Kyle Shores <kyle.shores44@gmail.com>
2019-01-03 16:30:20 -0600

    using decouple to hide secrets

--- tambda/settings.py
+++ tambda/settings.py
@@ -11,22 +11,22 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config, Csv
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7yblioi@f5zg)(vlm7&6@fep73as9cz@@1vz+-ktj!b^-!b%fh'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['k20shores.pythonanywhere.com', '127.0.0.1']
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
@@ -79,20 +79,10 @@ WSGI_APPLICATION = 'tambda.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'k20shores$tambda',
-        'USER': 'k20shores',
-        'PASSWORD': 'jlfBw5uybuS5O!5mw3yn',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    },
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
