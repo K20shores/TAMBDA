db730ae9cabafde9523f6d898d9aa6ffe7d56aa7
Kyle Shores <kyle.shores44@gmail.com>
2019-01-22 20:38:22 -0600

    starting to pull in google authentication

--- tambda/settings.py
+++ tambda/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -136,4 +139,13 @@ LOGIN_URL = 'login'
 
 TEMPLATE_LOADERS = (
     'django.template.loaders.app_directories.load_template_source',
+)
+
+AUTHENTICATION_BACKENDS = (
+ 'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+ 'social_core.backends.google.GoogleOpenId',  # for Google authentication
+ 'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+# 'social_core.backends.github.GithubOAuth2',  # for Github authentication
+# 'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
+ 'django.contrib.auth.backends.ModelBackend',
 )
